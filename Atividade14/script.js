a4fd79028d6c0c14d35b495b4a05a070cd39e7cc
Antonio01101 <129549926+Antonio01101@users.noreply.github.com>
2026-08-05 14:35:01 -0300

    a

--- Atividade14/script.js
+++ Atividade14/script.js
@@ -1,24 +1,24 @@
 function validar() {
 
-    // Utilizando document.nomeform.elements[]
+
     let nome = document.nomeform.elements["nome"].value;
     let email = document.nomeform.elements["email"].value;
     let comentario = document.nomeform.elements["comentario"].value;
     let pesquisa = document.nomeform.elements["pesquisa"];
 
-    // Nome
+
     if (nome.length < 10) {
         alert("O nome deve ter pelo menos 10 caracteres.");
         return false;
     }
 
-    // Comentário
+
     if (comentario.length < 20) {
         alert("O comentário deve ter no mínimo 20 caracteres.");
         return false;
     }
 
-    // Pesquisa obrigatória
+
     let respondeu = false;
     let primeiraVez = false;
 
